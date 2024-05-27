05c922db1067366f9dd5f61a7c55f6ce8f52d236
Yi-Cheng Chuang <ychuang@wm.edu>
2024-05-27 13:45:00 -0400

    Update major_radius_modify.py

--- major_radius_modify.py
+++ major_radius_modify.py
@@ -3,40 +3,122 @@
 import numpy as np
 import re
 
-file ='C:/Users/user/Documents/SOLPS data/simulation data/mast/g027205.00275.X2.equ'
 
-with open(file) as f:
-     datalist =f.readlines()
+simu_loc = 'C:/Users/ychuang/Documents/SOLPS_data/simulation_data/mast/027205'
+simu_case = 'AD3D'
+file_dir = '{}/{}/baserun'.format(simu_loc, simu_case)
+equfile_dir = '{}/g027205_0275.X2.equ'.format(file_dir)
 
-j= 17
 
-while j< 43:
+
+
+shift_value = 0.55
+
+
+
+# file = 'C:/Users/user/Documents/SOLPS data/simulation data/mast/g027205.00275.X2.equ'
+
+with open(equfile_dir) as f:
+     datalist = f.readlines()
+
+
+def line_index_finder(file_lines):
+    
+    index_dic = {}
+    
+    for j, lines in enumerate(file_lines):
+    
+        if 'r(1:jm);' in lines:
+            print(j)
+            
+            index_dic['r'] = j+1
+        
+        elif 'z(1:km);' in lines:
+            print(j)
+            
+            index_dic['z'] = j-1
+        
+        elif 'rtf' in lines:
+            print(j)
+            
+            index_dic['Btaxis'] = j
+        
+        else:
+            pass
+    
+    return index_dic
+
+
+index_dic = line_index_finder(datalist)
+print(index_dic)
+
+
+bt = index_dic['Btaxis']
+text = datalist[bt]
+number = re.findall('[-+]?\d+\.\d+', text)
+print(number)
+
+
+
+j= index_dic['r']
+
+while j< index_dic['z']:
     string2 = datalist[j]
     gdata = re.findall('[-+]?\d+\.?\d+[eE]?[-+]\d+', string2)
     mylist =[]
     for ii in gdata:
-        a = np.float64(ii) +np.float64(0.30000000)
+        a = np.float64(ii) +np.float64(shift_value)
         b = "{:.8E}".format(a)
         mylist.append(b)
     writelist = ' '.join(str(x)+"   " for x in mylist)
-    datalist[j] = "\t" + writelist + "\n"
+    datalist[j] = '    ' + writelist + "\n"
     j= j+ 1
 
-file2 = 'C:/Users/user/Documents/SOLPS data/simulation data/mast/g027205.00275.X2_m.equ'
-with open(file2,'w') as g:
+m_equfile = '{}/g027205.00275.X2_m.equ'.format(file_dir)
+
+
+
+
+with open(m_equfile,'w') as g:
     for i,line in enumerate(datalist,0):         ## STARTS THE NUMBERING FROM 1 (by default it begins with 0)    
         g.writelines(line)
 
-w_file ='C:/Users/user/Documents/SOLPS data/simulation data/mast/vvfile.ogr'
 
-with open(w_file) as wf:
+def replacetext(search_text, replace_text):
+    with open(m_equfile, "r+") as f:
+        file_content = f.read()
+        file_content = re.sub(search_text, replace_text, file_content)
+        f.seek(0)
+        f.write(file_content)
+        f.truncate()
+        return "Text replaced"
+
+search_text = number[0]
+
+shift_axis = np.float64(number[0]) + shift_value
+print(shift_axis)
+
+replace_text = '{:.17f}'.format(shift_axis)
+print(replacetext(search_text = search_text, replace_text = replace_text))
+
+
+
+
+vessel_file = '{}/vvfile.ogr'.format(file_dir)
+
+
+
+with open(vessel_file) as wf:
      w_datalist = wf.readlines()
+
+
+vessel_value = shift_value*1000
 
 for m, k in enumerate(w_datalist):
 #for m in range(10):
     string_w = w_datalist[m]
     w_data = re.findall('[-+]?\d+\.?\d+', string_w)  
-    a = np.float64(w_data[0]) + np.float64(300.00000)
+    a = np.float64(w_data[0]) + np.float64(vessel_value)
     a1 = "{: .5f}".format(a)
     b = np.float64(w_data[1])
     b1 = "{: .5f}".format(b)
@@ -46,8 +128,11 @@ for m, k in enumerate(w_datalist):
     w_writelist = ' '.join(str(y)+ "\t  " for y in w_list)
     w_datalist[m] = "  " + w_writelist + "\n"
    
- 
-w_file2 = 'C:/Users/user/Documents/SOLPS data/simulation data/mast/vvfile_m.ogr'
-with open(w_file2,'w') as wg:
+
+
+m_vesselfile = '{}/vvfile_m.ogr'.format(file_dir)
+
+
+with open(m_vesselfile,'w') as wg:
     for l,w_line in enumerate(w_datalist):         ## STARTS THE NUMBERING FROM 1 (by default it begins with 0)    
         wg.writelines(w_line)
